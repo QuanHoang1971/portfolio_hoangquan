bf3adff2c7f83a2c9ea7144c27cfd38cb303486b
QuanHoang1971 <goodfriend3010@gmail.com>
2022-10-11 20:28:48 +0900

    first commit

---
+++ src/components/Work.jsx
@@ -0,0 +1,174 @@
+import React from "react";
+import PizzaImg from "../assets/pizza.png";
+import Money from "../assets/money.png";
+import Pokemon from "../assets/Pokemon.png";
+import Yumy from "../assets/yumy.png";
+import PS5 from "../assets/ps5.png";
+
+const Work = () => {
+  return (
+    <div
+      name="work"
+      className="w-full md:h-screen text-gray-300 text-lg bg-[#2f1e1e]"
+    >
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-red-600">
+            Work
+          </p>
+          <p className="py-6 text-xl ">Check out some of my recent work</p>
+        </div>
+
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Pizza */}
+          <div
+            style={{ backgroundImage: `url(${PizzaImg})` }}
+            className="shadow-lg  shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Crostata Pizza
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://www.youtube.com/watch?v=HiEXUR98hzE&t=36s"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/QuanHoang1971/CrostataPizza"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Money */}
+          <div
+            style={{ backgroundImage: `url(${Money})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Money PiPi{" "}
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://www.youtube.com/watch?v=BvbFLolZDl0"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/QuanHoang1971/Money_Manager_Expense"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Pokemon */}
+          <div
+            style={{ backgroundImage: `url(${Pokemon})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Pokemon ReactJS
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://quanhoang1971.github.io/Pokemon/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/QuanHoang1971/Pokemon"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Yumy})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Yummy Yummy
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://quanhoang1971.github.io/YumyYumy/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/QuanHoang1971/YumyYumy"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid PS5 */}
+          <div
+            style={{ backgroundImage: `url(${PS5})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                PlayStaiton 5 LP
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://quanhoang1971.github.io/PlayStation5/"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/QuanHoang1971/PlayStation5"
+                  target="_blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
